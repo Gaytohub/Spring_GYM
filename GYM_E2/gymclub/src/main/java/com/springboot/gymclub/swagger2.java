7aa4470f0cc9b19c37a47adb410e6b3ab900b125
Gaytohub <tiamo39@vip.qq.com>
2019-05-13 20:25:58 +0800

    Create swagger2.java

---
+++ GYM_E2/gymclub/src/main/java/com/springboot/gymclub/swagger2.java
@@ -0,0 +1,43 @@
+package com.springboot.gymclub;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())//调用apiInfo方法,创建一个ApiInfo实例,里面是展示在文档页面信息内容
+                .select()
+                //控制暴露出去的路径下的实例
+                //如果某个接口不想暴露,可以使用以下注解
+                //@ApiIgnore 这样,该接口就不会暴露在 swagger2 的页面下
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.gymclub"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("gymclub restful api文档")
+                //创建人
+                .contact("jerry peng")
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
+
+}
