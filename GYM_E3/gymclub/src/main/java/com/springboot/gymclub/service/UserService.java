0a3f0dd79f356f681f0cb8215a52a2c723d53677
Gaytohub <tiamo39@vip.qq.com>
2019-06-05 12:50:54 +0800

    Create UserService.java

---
+++ GYM_E3/gymclub/src/main/java/com/springboot/gymclub/service/UserService.java
@@ -0,0 +1,15 @@
+package com.springboot.gymclub.service;
+
+import com.springboot.gymclub.entity.Course;
+import com.springboot.gymclub.entity.User;
+import com.springboot.gymclub.entity.UserCourse;
+
+import java.util.List;
+
+public interface UserService {
+    User findByPhone(Long phone);
+    User findByPhoneAndPassword(Long phone,String password);
+    void addUser(String username,String password,String gender,Long phone);
+    boolean tryAcquireSeckill();
+    List<UserCourse> getUserCourse(Long phone);
+}
